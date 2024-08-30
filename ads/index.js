d26b16501b79ac46d0c2f032819ba60a97991861
ismailmdn <151222970+ismailmdn@users.noreply.github.com>
2024-08-30 20:52:13 +0000

    commit

---
+++ ads/index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+let items = [
+  { id: '1', name: 'Item 1' ,nameId:"app1name",data:{apsk:"djd", sock:"dsdnks",hbjsdbni:"mmm"}},
+  { id: '2', name: 'Item 2' ,nameId:"app2name",data:{apsk:"djd", sock:"dsdnks",hbjsdbni:"mmm"}},
+  { id: '3', name: 'Item 3' ,nameId:"app3name",data:{apsk:"djd", sock:"dsdnks",hbjsdbni:"mmm"}}
+];
+
+// GET all items
+app.get('/items', (req, res) => {
+  res.json(items);
+});
+
+// GET a single item by ID
+app.get('/items/:id', (req, res) => {
+  const item = items.find(i => i.id === req.params.id);
+ res.json(item);
+});
+// GET a single item by ID
+app.get('/itemsByName/:name', (req, res) => {
+  const item = items.find(i => i.nameId === req.params.name);
+ res.json(item!=null?item.data:item);
+});
+
+// POST a new item
+app.post('/items', (req, res) => {
+  const newItem = {
+    id: items.length + 1,
+    name: req.body.name,
+  };
+  items.push(newItem);
+  res.status(201).json(newItem);
+});
+
+// PUT (update) an existing item
+app.put('/items/:id', (req, res) => {
+  const item = items.find(i => i.id === parseInt(req.params.id));
+  if (!item) return res.status(404).send('Item not found');
+
+  item.name = req.body.name;
+  res.json(item);
+});
+
+// DELETE an item
+app.delete('/items/:id', (req, res) => {
+  items = items.filter(i => i.id !== parseInt(req.params.id));
+  res.status(204).send();
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
