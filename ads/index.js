ac935f601a3566834cb537e27531571d42fda47b
ismailmdn <151222970+ismailmdn@users.noreply.github.com>
2024-09-10 18:06:50 +0000

    Your commit message

--- ads/index.js
+++ ads/index.js
@@ -28,7 +28,7 @@ app.get('/itemsByName/:name', (req, res) => {
 
 
 app.get('/data/koraApp', (req, res) => {
-  res.status(201).json({})});
+  res.status(201).json({"AD_NETWORK":"admob","OPEN_ADS":"ca-app-pub-9073960365383029/1979732165","ADMOB_BANNER_HOME":"ca-app-pub-9073960365383029/2813267843","ADMOB_BANNER_DETAIL":"ca-app-pub-9073960365383029/1500186173","ADMOB_INTER_SPLASH":"ca-app-pub-9073960365383029/9961716858","ADMOB_INTER_DETAIL":"ca-app-pub-9073960365383029/1169013683"})});
 
 
 app.get('/key', (req, res) => {
