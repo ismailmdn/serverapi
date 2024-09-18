9bdb14287b36f923c0c415c543bd0613344dd63e
ismailmdn <151222970+ismailmdn@users.noreply.github.com>
2024-09-18 18:42:15 +0000

    Your descriptive commit message

--- ads/index.js
+++ ads/index.js
@@ -27,11 +27,11 @@ app.get('/itemsByName/:name', (req, res) => {
 });
 
 
-app.get('/data/koraApp', (req, res) => {
+app.get('/koraApp/data', (req, res) => {
   res.status(201).json({"AD_NETWORK":"admob","OPEN_ADS":"ca-app-pub-9073960365383029/1979732165","ADMOB_BANNER_HOME":"ca-app-pub-9073960365383029/2813267843","ADMOB_BANNER_DETAIL":"ca-app-pub-9073960365383029/1500186173","ADMOB_INTER_SPLASH":"ca-app-pub-9073960365383029/9961716858","ADMOB_INTER_DETAIL":"ca-app-pub-9073960365383029/1169013683"})});
 
 
-app.get('/key', (req, res) => {
+app.get('/koraApp/key', (req, res) => {
   res.status(201).json({ "key": 'c2fbdbb871a69e4c09680b10e776f37ee276f22f9fb60c4acdfde67787e57349' })});
 
 // PUT (update) an existing item
