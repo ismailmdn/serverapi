6eaf88a7342579ed90a35fb86bc963b2956dd5c4
ismailmdn <151222970+ismailmdn@users.noreply.github.com>
2024-09-22 23:37:51 +0000

    Your descriptive commit message

--- ads/Routes/koraRoutes.js
+++ ads/Routes/koraRoutes.js
@@ -4,14 +4,7 @@ const router = express.Router();
 
 // Kora App Routes
 router.get('/data', (req, res) => {
-  res.status(201).json({
-    "AD_NETWORK": "admob",
-    "OPEN_ADS": "ca-app-pub-9073960365383029/1979732165",
-    "ADMOB_BANNER_HOME": "ca-app-pub-9073960365383029/2813267843",
-    "ADMOB_BANNER_DETAIL": "ca-app-pub-9073960365383029/1500186173",
-    "ADMOB_INTER_SPLASH": "ca-app-pub-9073960365383029/9961716858",
-    "ADMOB_INTER_DETAIL": "ca-app-pub-9073960365383029/1169013683"
-  });
+  res.status(201).json({});
 });
 
 router.get('/key', (req, res) => {
--- ads/index.js
+++ ads/index.js
@@ -3,14 +3,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Import the Kora routes
+const koraRoutes = require('./koraRoutes');
 
-const koraRoutes = require('./Routes/koraRoutes');
-
-
+// Middleware to parse JSON
 app.use(express.json());
 
+// Use the Kora App routes
 app.use('/koraApp', koraRoutes);
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
