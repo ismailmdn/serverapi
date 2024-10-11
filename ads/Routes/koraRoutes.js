3fa6676b5663b5996745e3dcaf821ef1bb81a6a4
ismailmdn <151222970+ismailmdn@users.noreply.github.com>
2024-10-11 20:14:42 +0000

    ads

--- ads/Routes/koraRoutes.js
+++ ads/Routes/koraRoutes.js
@@ -4,7 +4,8 @@ const router = express.Router();
 
 // Kora App Routes
 router.get('/data', (req, res) => {
-  res.status(201).json({});
+  res.status(201).json({"AD_NETWORK":"admob","OPEN_ADS":"ca-app-pub-7676093752347604/1463193930","ADMOB_BANNER_HOME":"ca-app-pub-7676093752347604/7124979493","ADMOB_BANNER_DETAIL":"ca-app-pub-7676093752347604/5214736682","ADMOB_INTER_SPLASH":"ca-app-pub-7676093752347604/2588573343","ADMOB_INTER_DETAIL":"ca-app-pub-7676093752347604/6248616753"}
+  );
 });
 
 router.get('/key', (req, res) => {
