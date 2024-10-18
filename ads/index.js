ae8fd4c241d87678c8a9b493aab3e28174fdf855
ismailmdn <151222970+ismailmdn@users.noreply.github.com>
2024-10-18 16:00:52 +0000

    news

--- ads/index.js
+++ ads/index.js
@@ -5,12 +5,13 @@ const port = 3000;
 
 // Import the Kora routes
 const koraRoutes = require('./Routes/koraRoutes');
-
+const FileManagerRoutes = require('./Routes/FileManagerRoutes');
 // Middleware to parse JSON
 app.use(express.json());
 
 // Use the Kora App routes
 app.use('/koraApp', koraRoutes);
+app.use('/FileManagerApp', FileManagerRoutes);
 
 // Start the server
 app.listen(port, () => {
