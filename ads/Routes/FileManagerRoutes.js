c60158ed525ef806103e402d02f334e795408c2d
ismailmdn <151222970+ismailmdn@users.noreply.github.com>
2025-01-31 12:30:02 +0000

    new

--- ads/Routes/FileManagerRoutes.js
+++ ads/Routes/FileManagerRoutes.js
@@ -4,14 +4,7 @@ const router = express.Router();
 
 // Kora App Routes
 router.get('/data', (req, res) => {
-  res.status(201).json({
-    "AD_NETWORK": "admob",
-    "OPEN_ADS": "ca-app-pub-7676093752347604/4885661090",
-    "ADMOB_BANNER_HOME": "ca-app-pub-7676093752347604/6637726467",
-    "ADMOB_BANNER_DETAIL": "ca-app-pub-7676093752347604/6333468541",
-    "ADMOB_INTER_SPLASH": "ca-app-pub-7676093752347604/3707305208",
-    "ADMOB_INTER_DETAIL": "ca-app-pub-7676093752347604/6633857032"
-  });
+  res.status(201).json({});
 });
 
 module.exports = router;
