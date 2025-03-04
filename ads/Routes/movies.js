dd344778eedb70bf35c5148ffdc3e662140319fa
ismailmdn <151222970+ismailmdn@users.noreply.github.com>
2025-03-04 02:14:16 +0000

    return adsmovies

--- ads/Routes/movies.js
+++ ads/Routes/movies.js
@@ -4,7 +4,11 @@ const router = express.Router();
 
 // Kora App Routes
 router.get('/data', (req, res) => {
-  res.status(201).json();
+  res.status(201).json({
+    "OPEN_ADS": "ca-app-pub-7676093752347604/8023160988",
+    "ADMOB_INTER_SPLASH": "ca-app-pub-7676093752347604/4306271666",
+    "ADMOB_INTER_DETAIL": "ca-app-pub-7676093752347604/6775053726"
+  });
 });
 
 module.exports = router;
